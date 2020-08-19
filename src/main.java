b0244810258e387210591fba2267972f13903202
Goglbum <goglbum@gmail.com>
2020-08-19 18:21:42 +0300

    Money Transfer

---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+
+    public static void main(String[] args) {
+        int cash = 2000000000;
+        int transfer = 500000000;
+        int totalCash;
+        totalCash = cash - transfer;
+        System.out.println(totalCash);
+    }
+}
